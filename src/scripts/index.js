5320d5f36bbfe5487c4356bb0df53d4585132c19
maxyuri99 <maxyurisantos@hotmail.com>
2023-06-14 19:50:53 -0300

    Novo Commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,13 @@
-import { posts, users } from './database.js';
-import { suggestUsers } from './database.js';
-import { closeModal, closeModalOutlineClick, createModal } from './modal.js';
-import { render } from './render.js';
+import { posts, users } from './database.js'
+import { suggestUsers } from './database.js'
+import { closeModal, closeModalOutlineClick, createModal } from './modal.js'
+import { render } from './render.js'
 
 export const handleModal = () => {
   const modalController = document.querySelector('.modal__controller')
   const buttonPost = document.querySelectorAll('.button__post')
 
-  for (let i = 0; i < buttonPost.length; i++) {
-    const button = buttonPost[i]
-
+  buttonPost.forEach((button) => {
     button.addEventListener('click', (event) => {
       modalController.innerHTML = ''
 
@@ -22,40 +20,39 @@ export const handleModal = () => {
       closeModal()
       closeModalOutlineClick()
     })
-  }
+  })
 }
 
 const insertPost = () => {
   const buttonForm = document.querySelector('.button__form')
   const inputTitle = document.querySelector('.input__form')
   const inputText = document.querySelector('.text__form')
-  const validationMessages = document.querySelector('.validation-message');
+  const validationMessages = document.querySelector('.validation-message')
 
   buttonForm.addEventListener('click', (event) => {
     const clientId = Number(event.target.dataset.clientId)
 
-    event.preventDefault();
+    event.preventDefault()
 
     if (inputTitle.value === '' || inputText.value === '') {
-      validationMessages.style.display = 'block';
-      validationMessages.textContent = 'Por favor, preencha todos os campos.';
-      return;
+      validationMessages.style.display = 'block'
+      validationMessages.textContent = 'Por favor, preencha todos os campos.'
+      return
     }
 
     let newValue = {}
-    console.log(event.target.dataset.clientId)
     const userSearch = users.find((user) => user.id === clientId)
 
-    newValue.user = userSearch.user;
-    newValue.stack = userSearch.stack;
-    newValue.img = userSearch.img;
-    newValue.title = inputTitle.value;
-    newValue.text = inputText.value;
-    newValue.id = posts.length + 1;
+    newValue.user = userSearch.user
+    newValue.stack = userSearch.stack
+    newValue.img = userSearch.img
+    newValue.title = inputTitle.value
+    newValue.text = inputText.value
+    newValue.id = posts.length + 1
     newValue.likes = 0
     posts.push(newValue)
 
-    validationMessages.style.display = 'none';
+    validationMessages.style.display = 'none'
 
     inputTitle.value = ''
     inputText.value = ''
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,36 +1,35 @@
-import { handleModal } from "./index.js";
+import { handleModal } from "./index.js"
 
 export function render(arrayDatabase, typeList) {
-  const listPosts = document.querySelector(".main-list__posts");
-  const listSuggestions = document.querySelectorAll(".main-list__suggestions");
+  const listPosts = document.querySelector(".main-list__posts")
+  const listSuggestions = document.querySelectorAll(".main-list__suggestions")
 
   if (typeList === 1) {
-    listPosts.innerHTML = "";
+    listPosts.innerHTML = ""
   } else {
     listSuggestions.forEach((listSuggestion) => {
-      listSuggestion.innerHTML = "";
-    });
+      listSuggestion.innerHTML = ""
+    })
   }
 
-  for (let i = 0; i < arrayDatabase.length; i++) {
-    const userArray = arrayDatabase[i];
-    const card = createCard(userArray, typeList);
+  arrayDatabase.forEach((arrayRender) => {
+    const card = createCard(arrayRender, typeList)
 
     if (typeList === 1) {
-      listPosts.appendChild(card);
+      listPosts.appendChild(card)
     } else {
       listSuggestions.forEach((listSuggestion) => {
-        const clonedCard = card.cloneNode(true);
-        listSuggestion.appendChild(clonedCard);
-        
-        const buttonSuggestionClick = clonedCard.querySelector(".button__suggestion");
+        const clonedCard = card.cloneNode(true)
+        listSuggestion.appendChild(clonedCard)
+
+        const buttonSuggestionClick = clonedCard.querySelector(".button__suggestion")
         buttonSuggestionClick.addEventListener("click", function () {
-          toggleButton(buttonSuggestionClick);
-        });
-      });
+          toggleButton(buttonSuggestionClick)
+        })
+      })
     }
-    
-  }
+
+  })
 
   if (typeList === 1) {
     handleModal()
@@ -40,107 +39,107 @@ export function render(arrayDatabase, typeList) {
 
 function createCard(userArray, typeList) {
   if (typeList === 1) {
-    const listPosts = document.createElement("li");
-    const userContainer = document.createElement("section");
-    const userDesc = document.createElement("div");
-    const imgUser = document.createElement("img");
-    const nameUser = document.createElement("h2");
-    const stackUser = document.createElement("p");
-    const titlePost = document.createElement("h3");
-    const textPost = document.createElement("p");
-    const buttonsContainer = document.createElement("div");
-    const buttonPost = document.createElement("button");
-    const heartContainerDiv = document.createElement("div");
-    const heartPost = document.createElement("img");
-    const numberHearts = document.createElement("p");
+    const listPosts = document.createElement("li")
+    const userContainer = document.createElement("section")
+    const userDesc = document.createElement("div")
+    const imgUser = document.createElement("img")
+    const nameUser = document.createElement("h2")
+    const stackUser = document.createElement("p")
+    const titlePost = document.createElement("h3")
+    const textPost = document.createElement("p")
+    const buttonsContainer = document.createElement("div")
+    const buttonPost = document.createElement("button")
+    const heartContainerDiv = document.createElement("div")
+    const heartPost = document.createElement("img")
+    const numberHearts = document.createElement("p")
 
-    listPosts.classList.add("list__posts");
-    userContainer.classList.add("desc-user__container");
-    userDesc.classList.add("user__desc");
-    imgUser.classList.add("user__img");
+    listPosts.classList.add("list__posts")
+    userContainer.classList.add("desc-user__container")
+    userDesc.classList.add("user__desc")
+    imgUser.classList.add("user__img")
 
-    imgUser.src = userArray.img;
-    nameUser.innerText = userArray.user;
-    stackUser.innerText = userArray.stack;
+    imgUser.src = userArray.img
+    nameUser.innerText = userArray.user
+    stackUser.innerText = userArray.stack
 
-    titlePost.innerText = userArray.title;
-    textPost.innerText = userArray.text;
+    titlePost.innerText = userArray.title
+    textPost.innerText = userArray.text
 
-    buttonsContainer.classList.add("post-button__container");
-    buttonPost.classList.add("button__post");
+    buttonsContainer.classList.add("post-button__container")
+    buttonPost.classList.add("button__post")
     heartContainerDiv.classList.add("heart__container")
-    heartPost.classList.add("post__heart");
+    heartPost.classList.add("post__heart")
 
-    heartPost.src = "./src/assets/img/heart-grey.png";
-    heartPost.alt = "heart-grey";
-    buttonPost.innerText = "Abrir Post";
-    buttonPost.dataset.clientId = userArray.id;
-    numberHearts.innerText = `${userArray.likes}`;
+    heartPost.src = "./src/assets/img/heart-grey.png"
+    heartPost.alt = "heart-grey"
+    buttonPost.innerText = "Abrir Post"
+    buttonPost.dataset.clientId = userArray.id
+    numberHearts.innerText = `${userArray.likes}`
 
-    listPosts.append(userContainer, titlePost, textPost, buttonsContainer);
-    userContainer.append(imgUser, userDesc);
-    userDesc.append(nameUser, stackUser);
-    buttonsContainer.append(buttonPost, heartContainerDiv);
+    listPosts.append(userContainer, titlePost, textPost, buttonsContainer)
+    userContainer.append(imgUser, userDesc)
+    userDesc.append(nameUser, stackUser)
+    buttonsContainer.append(buttonPost, heartContainerDiv)
     heartContainerDiv.append(heartPost, numberHearts)
 
     heartContainerDiv.addEventListener("click", function () {
       if (heartPost.src.includes("heart-grey")) {
-        heartPost.src = "./src/assets/img/heart-red.png";
-        heartPost.alt = "heart-red";
-        userArray.likes++;
+        heartPost.src = "./src/assets/img/heart-red.png"
+        heartPost.alt = "heart-red"
+        userArray.likes++
       } else {
-        heartPost.src = "./src/assets/img/heart-grey.png";
-        heartPost.alt = "heart-grey";
-        userArray.likes--;
+        heartPost.src = "./src/assets/img/heart-grey.png"
+        heartPost.alt = "heart-grey"
+        userArray.likes--
       }
-      numberHearts.textContent = userArray.likes;
-    });
+      numberHearts.textContent = userArray.likes
+    })
 
-    return listPosts;
+    return listPosts
   } else {
-    const listSuggestion = document.createElement("li");
-    const userContainer = document.createElement("section");
-    const userDesc = document.createElement("div");
-    const imgUser = document.createElement("img");
-    const nameUser = document.createElement("h2");
-    const stackUser = document.createElement("p");
-    const buttonSuggestion = document.createElement("button");
+    const listSuggestion = document.createElement("li")
+    const userContainer = document.createElement("section")
+    const userDesc = document.createElement("div")
+    const imgUser = document.createElement("img")
+    const nameUser = document.createElement("h2")
+    const stackUser = document.createElement("p")
+    const buttonSuggestion = document.createElement("button")
 
-    listSuggestion.classList.add("list__suggestions");
-    userContainer.classList.add("desc-user__container");
-    userDesc.classList.add("user__desc");
-    imgUser.classList.add("user__img");
-    buttonSuggestion.classList.add("button__suggestion");
+    listSuggestion.classList.add("list__suggestions")
+    userContainer.classList.add("desc-user__container")
+    userDesc.classList.add("user__desc")
+    imgUser.classList.add("user__img")
+    buttonSuggestion.classList.add("button__suggestion")
 
-    imgUser.src = userArray.img;
-    nameUser.innerText = userArray.user;
-    stackUser.innerText = userArray.stack;
-    buttonSuggestion.innerText = "Seguir";
-    buttonSuggestion.clientid = userArray.id;
+    imgUser.src = userArray.img
+    nameUser.innerText = userArray.user
+    stackUser.innerText = userArray.stack
+    buttonSuggestion.innerText = "Seguir"
+    buttonSuggestion.clientid = userArray.id
 
-    listSuggestion.append(userContainer, buttonSuggestion);
-    userContainer.append(imgUser, userDesc);
-    userDesc.append(nameUser, stackUser);
+    listSuggestion.append(userContainer, buttonSuggestion)
+    userContainer.append(imgUser, userDesc)
+    userDesc.append(nameUser, stackUser)
 
-    const buttonSuggestionClick = listSuggestion.querySelector(".button__suggestion");
+    const buttonSuggestionClick = listSuggestion.querySelector(".button__suggestion")
 
     buttonSuggestionClick.addEventListener("click", function () {
-      toggleButton(buttonSuggestionClick);
-    });
+      toggleButton(buttonSuggestionClick)
+    })
 
-    return listSuggestion;
+    return listSuggestion
   }
 }
 
 const toggleButton = (button) => {
   if (button.innerText === "Seguir") {
-    button.innerText = "Seguindo";
-    button.classList.add("seguindo__suggestion");
-    button.classList.remove("button__suggestion");
+    button.innerText = "Seguindo"
+    button.classList.add("seguindo__suggestion")
+    button.classList.remove("button__suggestion")
   } else {
-    button.innerText = "Seguir";
-    button.classList.remove("seguindo__suggestion");
-    button.classList.add("button__suggestion");
+    button.innerText = "Seguir"
+    button.classList.remove("seguindo__suggestion")
+    button.classList.add("button__suggestion")
   }
 }
 
